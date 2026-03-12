ddfd392fc69ca4e9e16b67e3c2e18b79d8bd5c80
Gleb <60158541+OutTuna@users.noreply.github.com>
2026-03-12 10:38:49 +0200

    adding console clear
    adding console clear before output

--- Lab4_var1/zavd1.cpp
+++ Lab4_var1/zavd1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-using namespace std;
+#include <cstdlib>
+#include <cctype>
 
+using namespace std;
 
 struct Student {
     string NAME, GROUP;
@@ -19,48 +21,51 @@ double getAVG(const Student& s) {
 
 void printAllStudents(Student arr[], int n) {
     for (int i = 0; i < n; i++) {
-        cout << i+1 << ". "<< arr[i].NAME << " | Group "<< arr[i].GROUP << "| Grades: ";
+        cout << i + 1 << ". " << arr[i].NAME << " | Group " << arr[i].GROUP << " | Grades: ";
         for (int j = 0; j < 5; j++) {
             cout << arr[i].SES[j] << " ";
-            cout << "| avg grade: " << getAVG(arr[i]) << "\n";
         }
+        cout << "| avg grade: " << getAVG(arr[i]) << "\n";
     }
 }
 
 void printGoodStudents(Student arr[], int n) {
     bool found = false;
-    cout << "Students with grade > 4.0\n";
+    cout << "=== Students with grade > 4.0 ===\n";
     for (int i = 0; i < n; i++) {
         if (getAVG(arr[i]) > 4.0) {
-            cout << "Surname: " << arr[i].NAME << ", Group:" << arr[i].GROUP << "\n";
+            cout << "Surname: " << arr[i].NAME << ", Group: " << arr[i].GROUP << "\n";
             found = true;
         }
-    }if (!found) {
-        cout << "Didn't found any students with grade > 4.0\n";
     }
+    if (!found) {
+        cout << "Didn't find any students with grade > 4.0\n";
+    }
+    cout << "\n";
 }
 
 void printVowelStudents(Student arr[], int n) {
-    cout << "Student's which surnames on vowels \n";
+    cout << "=== Students whose surnames start with vowels ===\n";
     string vowels = "AEIOUY";
     bool found = false;
     for (int i = 0; i < n; i++) {
         if (!arr[i].NAME.empty()) {
-            if (vowels.find(arr[i].NAME)!= string::npos) {
-                cout << "Surname: " << arr[i].NAME << ", Group: " << arr[i].GROUP<< "AVG : "<< getAVG(arr[i]) << "\n";
+            if (vowels.find(toupper(arr[i].NAME[0])) != string::npos) {
+                cout << "Surname: " << arr[i].NAME << ", Group: " << arr[i].GROUP << " | AVG : " << getAVG(arr[i]) << "\n";
                 found = true;
             }
         }
     }
     if (!found) {
-        cout << "Didn't found any students.\n";
+        cout << "Didn't find any students.\n";
     }
+    cout << "\n";
 }
 
 void sortStudents(Student arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (getAVG(arr[i]) > getAVG(arr[j+1])) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (getAVG(arr[j]) > getAVG(arr[j+1])) {
                 Student temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
@@ -70,77 +75,85 @@ void sortStudents(Student arr[], int n) {
 }
 
 void deleteAfterMin(Student*& arr, int& n) {
-    if (n<=1) {
-        cout << "\n Not enought elements for delete";
+    if (n <= 1) {
+        cout << "Not enough elements to delete\n";
         return;
     }
-    int minAVG = getAVG(arr[0]), minIndex = 0;
+
+    double minAVG = getAVG(arr[0]);
+    int minIndex = 0;
 
     for (int i = 1; i < n; i++) {
-        int currentAVG = getAVG(arr[i]);
+        double currentAVG = getAVG(arr[i]);
         if (currentAVG < minAVG) {
             minAVG = currentAVG;
             minIndex = i;
         }
     }
-    cout << "\n Min AVG. " << arr[minIndex].NAME << " (" << minAVG << ")\n";
+    cout << "\nMin AVG: " << arr[minIndex].NAME << " (" << minAVG << ")\n";
 
     if (minIndex < n - 1) {
         int deleteIndex = minIndex + 1;
-        cout << "Delete this student" << arr[deleteIndex].NAME << "\n";
-        for (int i = deleteIndex; i < n; i++) {
+        cout << "Deleting this student: " << arr[deleteIndex].NAME << "\n\n";
+
+        for (int i = deleteIndex; i < n - 1; i++) {
             arr[i] = arr[i+1];
         }
         n--;
-    }else {
-        cout << "\n Non elements for delete, this is the smallest";
+    } else {
+        cout << "No elements to delete, the element with minimum AVG is the last one.\n\n";
     }
 }
 
+void clearConsole() {
+    system("clear");
+}
 
 int main () {
     int n;
-    cout << "Enter (n) Students :";
+    cout << "Enter (n) Students: ";
     cin >> n;
 
     if (n <= 0) {
-        cout << "Students must be bigger than 0";
-    return 0;
+        cout << "Students count must be greater than 0";
+        return 0;
     }
 
     Student* students = new Student[n];
 
     for (int i = 0; i < n; i++) {
-        cout << "Enter students data" << i+1 << "\n";
+        cout << "\n--- Enter students data " << i+1 << " ---\n";
         cout << "Surname: ";
-        cin.ignore();
-        getline(cin,students[i].NAME);
+        getline(cin >> ws, students[i].NAME);
 
         cout << "Group: ";
         cin >> students[i].GROUP;
 
-        cout << "Enter Grades(5) through space: ";
+        cout << "Enter Grades(5) separated by space: ";
         for (int j = 0; j < 5; j++) {
             cin >> students[i].SES[j];
         }
     }
-        printGoodStudents(students, n);
-        printVowelStudents(students, n);
 
-        cout << "\n before sort \n";
-        printVowelStudents(students, n);
+    clearConsole();
 
-        sortStudents(students, n);
+    printGoodStudents(students, n);
+    printVowelStudents(students, n);
 
-        cout << "\n after sort \n";
-        printAllStudents(students, n);
-        deleteAfterMin(students, n);
+    cout << "=== All Students (Before Sort) ===\n";
+    printAllStudents(students, n);
 
-        cout << "\n final after deleting \n";
-        printAllStudents(students, n);
+    sortStudents(students, n);
 
-        delete[] students;
+    cout << "\n=== All Students (After Sort by AVG) ===\n";
+    printAllStudents(students, n);
 
+    deleteAfterMin(students, n);
+
+    cout << "=== Final list (After Deleting) ===\n";
+    printAllStudents(students, n);
+
+    delete[] students;
 
     return 0;
 }
